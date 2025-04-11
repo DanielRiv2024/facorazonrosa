30d189f6a2c9a3c1b3ae8c2a872c18ac7ede3636
Daniel Rivera <rivera200224@gmail.com>
2025-04-10 18:16:44 -0600

    update v7

--- src/app/profile/user/page.js
+++ src/app/profile/user/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
-import Navbar from "../../app/navigation/navbar";
-import TopBar from "../../app/navigation/topbar";
+import Navbar from "../../../app/navigation/navbar";
+import TopBar from "../../../app/navigation/topbar";
 import { getUserFromCookies } from "../../../app/utils/auth";
 import { useRouter } from "next/navigation";
 import { CiSaveUp1 } from "react-icons/ci";
